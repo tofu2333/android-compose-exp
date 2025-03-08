f4a0c16e707715b21b27d1a0f768348d77aced5d
tofu <tofumilk@outlook.com>
2025-03-08 22:41:20 +0800

    任务管理器

---
+++ taskmanager/src/main/java/com/tofu/taskmanager/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.tofu.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tofu.taskmanager.ui.theme.AndroidComposeExpTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidComposeExpTheme {
+                App(
+                    modifier = Modifier
+                        .statusBarsPadding()
+                        .fillMaxSize()
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun App(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_task_completed),
+                contentDescription = null
+            )
+
+            Text(
+                modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+                text = stringResource(R.string.completed_text)
+            )
+
+            Text(
+                text = stringResource(R.string.nice_work_text),
+                fontSize = 16.sp
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun AppPreview() {
+    AndroidComposeExpTheme {
+        App()
+    }
+}
